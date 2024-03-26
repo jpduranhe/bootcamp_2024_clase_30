769b29b923fabe3b5d7c332653cba4496fb0e238
Juan Pablo Duran <pablo_duran@unicomer.com>
2024-03-25 21:17:52 -0300

    cargando la clase

---
+++ src/clase_30/Ejercicio.java
@@ -0,0 +1,55 @@
+package clase_30;
+
+public class Ejercicio {
+	
+	
+	/*
+	
+	1)Declara una variable de tipo char llamada "identificador" y asígnale el dígito verificador de tu RUT. 
+		Imprime esta variable.
+	2)Crea una variable de tipo boolean llamada "estudiante" y asígnale true si eres estudiante, 
+		false en caso contrario. Imprime esta variable..
+	3) Declara una variable String "apellido" y asígnale tu apellido. 
+		Imprime esta variable concatenada con tu nombre.
+
+	
+	*/
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+		// primitivos
+		short varShort1=99,
+			  varShort2=98;
+		
+		// entero - +
+		short varShort =0;
+		byte varByte =0;
+		int varInt =0;
+		long varLong =0;
+		
+		// parte decimal
+		double varDouble =0.0;
+		float varFloat =0.0f;
+		
+		
+		boolean varBoolen= true;
+		
+		char varChar='A';
+		
+		
+				
+		// objetos
+		Integer objInteger=null;
+		String objString= "Hola";
+		//char charFromString= objString.charAt(6);
+		System.out.println(varShort1);
+		System.out.println(varShort2);
+		Double d1=0.0;
+		Float f1=0.0f;
+		Short s=1;
+	}
+
+}
---
+++ src/clase_30/EjercicioOperador.java
@@ -0,0 +1,107 @@
+package clase_30;
+
+public class EjercicioOperador {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/*
+		 *  + 
+		 *  -
+		 *  *
+		 *  /
+		 *  %
+		 * 
+		 * 
+		 */
+		System.out.println("*****OPERADORES aritméticos****");
+		
+		int num1=10, num2=5, num3=20, num4=11;
+		
+		int resultadoSuma = num1+num2;
+		
+		System.out.println("resultado num1+num2= "+resultadoSuma);
+		
+		int resultadoResta = num1-num2;
+		
+		System.out.println("resultado num1-num2= "+resultadoResta);
+		
+		int resultadoMult = num1*num2;
+		
+		System.out.println("resultado num1*num2= "+resultadoMult);
+		
+		int resultadoDiv = num1/num2;
+		
+		System.out.println("resultado num1/num2= "+resultadoDiv);
+		
+		int resultadoMod = num1%num2;
+		
+		System.out.println("resultado num1%num2= "+resultadoMod);
+		
+		System.out.println();
+		System.out.println();
+		System.out.println("*****OPERADORES UNARIOS****");
+		/*
+		 * ++
+		 * --
+		 * 
+		 */
+		int resultadoPlusPlus = num1++;
+		System.out.println("resultado num1++= "+resultadoPlusPlus );
+		System.out.println("valor num1= "+num1 );
+		
+		int plusPlusresultado = ++num2;
+		System.out.println("resultado num1++= "+plusPlusresultado );
+		System.out.println("valor num1= "+num2 );
+		
+		
+		System.out.println("valor Original de num3= "+num3 );
+		System.out.println("resultado 10  + num3--  = "+(   10  + (num3-- ) ));
+		System.out.println("valor 10 + num3= "+  (10 + num3) );
+		
+		int resultadoMinusMinus = num3--;
+		System.out.println("resultado num3--= "+resultadoMinusMinus );
+		System.out.println("valor num3= "+num3 );
+		
+		
+		System.out.println();
+		System.out.println();
+		System.out.println("*****OPERADORES DE RELACIÓN****");
+		
+		/*
+		 *  ==
+		 *  !=
+		 *  
+		 *  <
+		 *  >
+		 * 	<=
+		 *  >=
+		 * 
+		 * 
+		 * */
+	
+		System.out.println("valores de num1  y de  num4 "+num1+" ==  "+num4 );
+		System.out.println("resultado num1 == num4 "+(  num1 == num4 ) );
+		
+		
+		System.out.println("valores de num1  y de  num2 "+num1+" == "+num2 );
+		System.out.println("resultado num1 == num2 "+(  num1 == num2 ) );
+		
+		
+		System.out.println("valores de num1  y de  num2 "+num1+" != "+num2 );
+		System.out.println("resultado num1 != num2 "+(  num1 != num2 ) );
+		
+		System.out.println("valores de num1  y de  num2 "+num1+" > "+num2 );
+		System.out.println("resultado num1 > num2 "+(  num1 > num2 ) );
+		
+		System.out.println("valores de num1  y de  num2 "+num1+" > "+num2 );
+		System.out.println("resultado num1 > num2 "+(  num1 > num2 ) );
+
+		System.out.println("valores de num1  y de  num2 "+num1+" < "+num2 );
+		System.out.println("resultado num1 < num2 "+(  num1 < num2 ) );
+		
+		
+		
+	}
+
+}
